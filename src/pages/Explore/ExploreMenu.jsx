c6507feda24d39cbaa8fec25a0a58e97534277b2
brunofeu <brunofeu89@gmail.com>
2021-11-08 19:28:20 -0300

    implementa a tela de explorar

--- src/pages/Explore/ExploreMenu.jsx
+++ src/pages/Explore/ExploreMenu.jsx
@@ -1,11 +1,26 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import Header from '../../components/Header';
 
 function ExploreMenu() {
+  const history = useHistory();
   return (
     <div>
       <Header title="Explorar" />
-      ExploreMenu
+      <button
+        type="button"
+        data-testid="explore-food"
+        onClick={ () => history.push('/explorar/comidas') }
+      >
+        Explorar Comidas
+      </button>
+      <button
+        type="button"
+        data-testid="explore-drinks"
+        onClick={ () => history.push('/explorar/bebidas') }
+      >
+        Explorar Bebidas
+      </button>
     </div>
   );
 }
