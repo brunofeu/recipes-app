7e41ce63f256c9171f248a7fe859a1d488cdc908
brunofeu <brunofeu89@gmail.com>
2021-11-08 19:53:20 -0300

    implementa os elementos de explorar bebidas e comidas

--- src/pages/Explore/ExploreDrinks.jsx
+++ src/pages/Explore/ExploreDrinks.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import Header from '../../components/Header';
 
 function ExploreDrinks() {
+  const history = useHistory();
   return (
     <div>
       <Header title="Explorar Bebidas" />
-      ExploreDrinks
+      <button
+        type="button"
+        data-testid="explore-by-ingredient"
+        onClick={ () => history.push('/explorar/bebidas/ingredientes') }
+      >
+        Por Ingredientes
+      </button>
+      {/* ajustar botao me surpreenda. necessita da API */}
+      <button
+        type="button"
+        data-testid="explore-surprise"
+        onClick={ () => history.push('/explorar/bebidas') }
+      >
+        Me Surpreenda!
+      </button>
     </div>
   );
 }
