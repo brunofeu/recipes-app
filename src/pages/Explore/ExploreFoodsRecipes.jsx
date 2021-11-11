e1069ffee4a33cc1c95bfecf619ed1577acbb337
brunofeu <brunofeu89@gmail.com>
2021-11-11 19:31:24 -0300

    adiciona card de ingredientes

--- src/pages/Explore/ExploreFoodsRecipes.jsx
+++ src/pages/Explore/ExploreFoodsRecipes.jsx
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import Header from '../../components/Header';
+import IngredientCard from '../../components/IngredientCard';
+import RecipeContext from '../../context/RecipeContext';
 
 function ExploreFoodsRecipes() {
+  const { fetchMeal } = useContext(RecipeContext);
+  const [ingredientsList, setIngredientsList] = useState();
+  const [isLoading, setIsloading] = useState(true);
+
+  const fetchIngredients = async () => {
+    setIsloading(true);
+    const INGREDIENTS_LIST_SIZE = 12;
+    const ingredients = await fetchMeal('list', 'i', 'list');
+    setIngredientsList(ingredients.slice(0, INGREDIENTS_LIST_SIZE));
+    setIsloading(false);
+  };
+
+  useEffect(() => {
+    fetchIngredients();
+  }, []);
+
   return (
     <div>
       <Header title="Explorar Ingredientes" />
-      ExploreFoodsRecipes
+      {!isLoading && <IngredientCard list={ ingredientsList } url="themealdb" /> }
     </div>
   );
 }
